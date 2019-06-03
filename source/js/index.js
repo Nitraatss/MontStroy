b36a6766cf8b6250a28d0b209d116006969f2f22
Vasilii <nitraatss@gmail.com>
2019-06-03 12:57:25 +0300

    Добавлен дропдаун для блока услуги

--- source/js/index.js
+++ source/js/index.js
@@ -1,5 +1,7 @@
 import initiateMenu from "./menu";
 import initiateMap from "./map";
+import initiateServices from "./services";
 
 initiateMenu();
+initiateServices();
 initiateMap();
---
+++ source/js/services.js
@@ -0,0 +1,32 @@
+const DESKTOP_WIDTH = 1200;
+
+const initiateServices = () => {
+  const services = document.querySelector(`.services`);
+  const singleServices = [].slice.call(
+      services.querySelectorAll(`.single-service`)
+  );
+
+  const toggleServiceList = (serviceTitle, serviceList) => {
+    serviceTitle.classList.toggle(`single-service__title--active`);
+    serviceList.classList.toggle(`single-service__list--active`);
+  };
+
+  singleServices.forEach((service, index) => {
+    const serviceTitle = service.querySelector(`.single-service__title`);
+    const serviceList = service.querySelector(`.single-service__list`);
+    serviceList.style.zIndex = `${40 - singleServices.length - index}`;
+
+    serviceTitle.addEventListener(`click`, () => {
+      toggleServiceList(serviceTitle, serviceList);
+    });
+
+    window.addEventListener(`resize`, () => {
+      if (window.innerWidth >= DESKTOP_WIDTH) {
+        serviceTitle.classList.remove(`single-service__title--active`);
+        serviceList.classList.remove(`single-service__list--active`);
+      }
+    });
+  });
+};
+
+export default initiateServices;
