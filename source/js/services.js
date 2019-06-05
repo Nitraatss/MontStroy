e701572067cfe31476e30b072e4bf3959856a5d4
Vasilii <nitraatss@gmail.com>
2019-06-05 16:35:34 +0300

    Исправление поведения блока услуги в планшетных и мобильных устройствах

--- source/js/services.js
+++ source/js/services.js
@@ -11,17 +11,23 @@ const initiateServices = () => {
     serviceList.classList.toggle(`single-service__list--active`);
   };
 
+  const hideServiceList = (serviceTitle, serviceList) => {
+    serviceTitle.classList.remove(`single-service__title--active`);
+    serviceList.classList.remove(`single-service__list--active`);
+  };
+
   singleServices.forEach((service, index) => {
     const serviceTitle = service.querySelector(`.single-service__title`);
     const serviceList = service.querySelector(`.single-service__list`);
-    serviceList.style.zIndex = `${40 - singleServices.length - index}`;
 
     serviceTitle.addEventListener(`click`, () => {
-      singleServices.forEach((service) => {
-        const serviceTitle = service.querySelector(`.single-service__title`);
-        const serviceList = service.querySelector(`.single-service__list`);
-        serviceTitle.classList.remove(`single-service__title--active`);
-        serviceList.classList.remove(`single-service__list--active`);
+      singleServices.forEach((service, subindex) => {
+        const subServiceTitle = service.querySelector(`.single-service__title`);
+        const subServiceList = service.querySelector(`.single-service__list`);
+
+        if (index !== subindex) {
+          hideServiceList(subServiceTitle, subServiceList);
+        }
       });
 
       toggleServiceList(serviceTitle, serviceList);
@@ -29,8 +35,7 @@ const initiateServices = () => {
 
     window.addEventListener(`resize`, () => {
       if (window.innerWidth >= DESKTOP_WIDTH) {
-        serviceTitle.classList.remove(`single-service__title--active`);
-        serviceList.classList.remove(`single-service__list--active`);
+        hideServiceList(serviceTitle, serviceList);
       }
     });
   });
